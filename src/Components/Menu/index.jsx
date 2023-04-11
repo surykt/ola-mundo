2c0e9ed84d5f2fbf6f7c47e25a403bf4ca7b479f
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-04-11 14:49:49 -0300

    change text decoration class name

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import styles from "./Menu.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Menu() {
+  const localization = useLocation();
+
+  console.log(localization);
+
   return (
     <header>
       <nav className={styles.navigation}>
-        <Link className={styles.link} to="/">
+        <Link
+          className={`${styles.link} ${
+            localization.pathname === "/" ? styles.featuredLink : ""
+          }`}
+          to="/"
+        >
           Home
         </Link>
-        <Link className={styles.link} to="/about-me">
+        <Link
+          className={`${styles.link} ${
+            localization.pathname === "/about-me" ? styles.featuredLink : ""
+          }`}
+          to="/about-me"
+        >
           About-me
         </Link>
       </nav>
