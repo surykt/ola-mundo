f504b4e07e282498f9e3c1e56824a0067d97d072
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-04-13 18:07:22 -0300

    add posts to home cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Banner from "Components/Banner";
+import styles from "./Home.module.css";
+import posts from "Json/posts.json";
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <ul className={styles.posts}>
+        {posts.map(post => {
+          return (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
