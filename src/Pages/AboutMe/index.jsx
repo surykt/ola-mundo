699dc23c47ffd5a7f55c767d9283b71157690ea9
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-04-19 19:09:54 -0300

    refactor post model

--- src/Pages/AboutMe/index.jsx
+++ src/Pages/AboutMe/index.jsx
@@ -1,5 +1,18 @@
+import PostModel from "Components/PostModel";
 import React from "react";
+import photoCover from "assets/sobre_mim_capa.png";
+import styles from "./AboutMe.module.css";
+import aboutMePhoto from "assets/myFoto.jpg";
 
 export default function AboutMe() {
-  return <h1>Sobre Mim ... </h1>;
+  return (
+    <PostModel photoCover={photoCover} title="Sobre Mim">
+      <h3 className={styles.subtitle}>Olá eu sou o Carlos Alan!</h3>
+      <img
+        src={aboutMePhoto}
+        alt="foto de Carlos Alan"
+        className={styles.aboutMePhoto}
+      />
+    </PostModel>
+  );
 }
